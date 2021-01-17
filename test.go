7089e449b8dab00b043d7f65e1dcdec614ce9092
Bill Ward <you@example.comfrom mac>
2021-01-17 08:42:18 -0600

    First commit.

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 12
+	fmt.Println(x)
+	x = 24 + 8
+	fmt.Println(x)
+	x = 24 * 8
+	fmt.Println(x)
+	x = 24 / 8
+	fmt.Println(x)
+	y := "Will"
+	fmt.Println(y)
+	y = "Bill"
+	fmt.Println(y)
+}
